3fb99d9d02c24a2ee771e92eeb2765ff61a55187
haih19 <haih19c@gmail.com>
2022-07-21 04:44:40 +0700

    update CRUD

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import ListTodo from './todos/ListTodo';
+import './todos/listTodo.scss'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
@@ -9,8 +13,22 @@ function App() {
         <p>
           Simple React List Todos
         </p>
+        <ListTodo></ListTodo>
 
       </header>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      {/* Same as */}
+      <ToastContainer />
     </div>
   );
 }
